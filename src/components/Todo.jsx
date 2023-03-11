26b1aabf8217e2d34cc2934de9b5e58ff7474c84
Ubeydullah YILMAZ <125451264+ubeyyilmaz@users.noreply.github.com>
2023-03-11 19:35:52 +0300

    Add files via upload

---
+++ src/components/Todo.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Todo = ({ todo, setTodos, todos }) => {
+  const deleteHandler = () => {
+    setTodos(todos.filter((el) => el.id !== todo.id));
+  };
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
+  return (
+    <div>
+      <li className={`${todo.completed ? "completed" : ""}`}>
+        <div className="view">
+          <input onClick={completeHandler} className="toggle" type="checkbox" />
+          <label>{todo.text}</label>
+          <button onClick={deleteHandler} className="destroy"></button>
+        </div>
+      </li>
+    </div>
+  );
+};
+
+export default Todo;
